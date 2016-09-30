0911a1710374aa00e1d4b257ca69495946dcbe6c
Taikatou <13123947@studentmail.ul.ie>
2016-09-30 12:33:51 +0100

    Yay fixed android version :)

---
+++ PureGame.Droid/Game1.cs
@@ -0,0 +1,34 @@
+using Microsoft.Xna.Framework;
+using MonoGame.Extended.Screens;
+using PureGame.Deploy.Screens;
+
+namespace PureGame.Droid
+{
+    public class Game1 : Game
+    {
+        // ReSharper disable once NotAccessedField.Local
+        private readonly GraphicsDeviceManager _graphicsDeviceManager;
+        public ScreenComponent ScreenComponent;
+
+        public Game1()
+        {
+            _graphicsDeviceManager = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+            Window.AllowUserResizing = true;
+            Components.Add(ScreenComponent = new ScreenComponent(this));
+        }
+
+        protected override void LoadContent()
+        {
+            ScreenComponent.Register(new GameScreen(Services, new ControllerSettings()));
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            base.Draw(gameTime);
+        }
+    }
+}
