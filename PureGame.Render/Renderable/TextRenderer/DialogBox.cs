f68907a480103dd38da8a43664575f11532712ae
Taikatou <13123947@studentmail.ul.ie>
2016-10-01 18:16:52 +0100

    Cleaned up dialog box

---
+++ PureGame.Render/Renderable/TextRenderer/DialogBox.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using PureGame.Client.Renderable.WorldRenderer;
+
+namespace PureGame.Client.Renderable.TextRenderer
+{
+    public class DialogBox
+    {
+        public readonly EntityRender RenderEntity;
+        public readonly Point Size;
+        public readonly Point Offset;
+        public Point Position
+        {
+            get
+            {
+                var position = RenderEntity.ScreenPosition - Offset;
+                return position;
+            }
+        }
+
+        public Rectangle TextBoxSpace => new Rectangle(Position, Size);
+
+        public bool Contains(Point point)
+        {
+            return TextBoxSpace.Contains(point);
+        }
+
+        public DialogBox(EntityRender renderEntity, Point size)
+        {
+            Size = size;
+            RenderEntity = renderEntity;
+            var offset = renderEntity.PositionFinder.Offset;
+            var xPos = Size.X/2 - (int)offset.X;
+            Offset = new Point(xPos, Size.Y);
+        }
+        public virtual void Draw(SpriteBatch spriteBatch, Texture2D textBoxTexture)
+        {
+            spriteBatch.Draw(textBoxTexture, TextBoxSpace, null, Color.White, 0.0f, new Vector2(0, 0), SpriteEffects.None, 0);
+        }
+    }
+}
--- PureGame.Render/Renderable/TextRenderer/PlainTextBoxRenderer.cs
+++ PureGame.Render/Renderable/TextRenderer/PlainTextBoxRenderer.cs
@@ -1,7 +1,6 @@
 ﻿using System.Diagnostics;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using MonoGame.Extended;
 using MonoGame.Extended.BitmapFonts;
 using PureGame.Engine.Communication;
 using PureGame.Client.Renderable.WorldRenderer;
@@ -11,27 +10,21 @@ namespace PureGame.Client.Renderable.TextRenderer
     public class PlainTextBoxRenderer : ITextBoxRenderer
     {
         private readonly string _text = "Interacting";
-        private readonly Vector2 _position;
-        private Rectangle _textBoxSpace;
         private readonly ITextBox _interaction;
         private readonly BitmapFont _font;
+        public DialogBox DialogBox;
 
         public PlainTextBoxRenderer(BitmapFont font, EntityRender r)
         {
             _font = font;
-            var offset = r.PositionFinder.Offset;
             _interaction = r.BaseEntity.Interaction;
-            _position = r.ScreenPosition.ToVector2() - offset * 2;
-            var textSpace = font.MeasureString(_text);
-            var textBoxSize = new Size((int)(textSpace.Width * 1.2), (int)(textSpace.Height * 1.2));
-            _textBoxSpace = new Rectangle(_position.ToPoint(), textBoxSize);
-            var difference = _textBoxSpace.Width - textSpace.Width;
-            _position.X += (int)(difference / 2);
+            var textSpace = font.GetSize(_text);
+            DialogBox = new DialogBox(r, textSpace);
         }
 
         public bool Tap(Vector2 position)
         {
-            var found = _textBoxSpace.Contains(position);
+            var found = DialogBox.Contains(position.ToPoint());
             Debug.WriteLine(position);
             if (found)
             {
@@ -42,8 +35,8 @@ namespace PureGame.Client.Renderable.TextRenderer
 
         public void Draw(SpriteBatch spriteBatch, Texture2D textBoxTexture)
         {
-            spriteBatch.Draw(textBoxTexture, _textBoxSpace, null, Color.White, 0.0f, new Vector2(0, 0), SpriteEffects.None, 0);
-            spriteBatch.DrawString(_font, _text, _position, Color.Black);
+            DialogBox.Draw(spriteBatch, textBoxTexture);
+            spriteBatch.DrawString(_font, _text, DialogBox.Position.ToVector2(), Color.Black);
         }
     }
 }
