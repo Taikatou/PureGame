e3278ac800e3b268038a9b385ea518fb260ce3d2
Taikatou <13123947@studentmail.ul.ie>
2016-10-04 00:17:18 +0100

    Started creating unit tests

---
+++ PureGame.Tests/NUnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace PureGame.Tests
+{
+    [TestFixture]
+    public class NUnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
